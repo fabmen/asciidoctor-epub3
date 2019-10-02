95addc15f501dea3ec70b53ef8aedf31e4254311
fabmen <menard.fabrice@gmail.com>
2019-10-02 08:36:21 +0200

    fix converter

--- lib/asciidoctor-epub3/converter.rb
+++ lib/asciidoctor-epub3/converter.rb
@@ -830,6 +830,41 @@ document.addEventListener('DOMContentLoaded', function(event, reader) {
     end
   end
 
+  def stem node
+    if (idx = node.subs.index :specialcharacters)
+      node.subs.delete_at idx
+      equation = node.content || ''
+      idx > 0 ? (node.subs.insert idx, :specialcharacters) : (node.subs.unshift :specialcharacters)
+    else
+      equation = node.content || ''
+    end
+    if node.style == 'asciimath'
+      # NOTE fop requires jeuclid to process mathml markup
+      equation_data = asciimath_available? ? ((::AsciiMath.parse equation).to_mathml 'mml:', 'xmlns:mml' => 'http://www.w3.org/1998/Math/MathML') : %(<mathphrase><![CDATA[#{equation}]]></mathphrase>)
+    else
+      # unhandled math; pass source to alt and required mathphrase element; dblatex will process alt as LaTeX math
+      equation_data = %(<alt><![CDATA[#{equation}]]></alt>
+<mathphrase><![CDATA[#{equation}]]></mathphrase>)
+    end
+    if node.title?
+      %(<equation#{common_attributes node.id, node.role, node.reftext}>
+<title>#{node.title}</title>
+#{equation_data}
+</equation>)
+    else
+      # WARNING dblatex displays the <informalequation> element inline instead of block as documented (except w/ mathml)
+      %(<informalequation#{common_attributes node.id, node.role, node.reftext}>
+#{equation_data}
+</informalequation>)
+    end
+  end
+  def asciimath_available?
+    (@asciimath_status ||= load_asciimath) == :loaded
+  end
+
+  def load_asciimath
+    (defined? ::AsciiMath.parse) ? :loaded : (Helpers.require_library 'asciimath', true, :warn).nil? ? :unavailable : :loaded
+  end
   def convert_content node
     node.content_model == :simple ? %(<p>#{node.content}</p>) : node.content
   end
